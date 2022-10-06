13d7e8d7031390979fe3c9c61b1fd3600e6e75e7
Hirota-Shunya-Fukuyama <gc22017@anabuki-net.ne.jp>
2022-10-06 11:42:23 +0900

    課題7　終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CModel.h
+++ 3DLv1_2022_vs2019/GameProgramming/src/CModel.h
@@ -1,5 +1,9 @@
 #ifndef CMODEL_H
 #define CMODEL_H
+//vectorのインクルード
+#include <vector>
+#include "CTriangle.h"
+
 /*
 モデルクラス
 モデルデータの入力や表示
@@ -9,6 +13,12 @@ public:
 	//モデルファイルの入力
 	//Load(モデルファイル名、マテリアルファイル名)
 	void Load(char* obj, char* mtl);
+	//描画
+	void Render();
+
+private:
+	//三角形の可変長配列
+	std::vector<CTriangle> mTriangles;
 };
 
 #endif
