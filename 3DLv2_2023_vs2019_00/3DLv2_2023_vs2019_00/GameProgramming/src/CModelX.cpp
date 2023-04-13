e2b60fd24967356a594e7c758eee5dffdfdc728d
Hirota-Shunya-Fukuyama <gc22017@anabuki-net.ne.jp>
2023-04-13 18:18:54 +0900

    課題2

--- 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "CModelX.h"
 #include "glut.h"
 
@@ -38,10 +39,80 @@ void CModelX::Load(char* file) {
 	//最後に\0を設定する(文字列の終端)
 	buf[size] = '\0';
 
-	printf("%s",buf);
+	//文字列の最後まで繰り返し
+	while (*mpPointer != '\0') {
+		GetToken();
+		//単語がFrameの場合
+		if (strcmp(mToken, "Frame") == 0) {
+			printf("%s ", mToken);
+			GetToken();
+			printf("%s\n", mToken);
+		}
+		if (strcmp(mToken, "AnimationSet") == 0) {
+			printf("%s ", mToken);
+			GetToken();
+			printf("%s\n", mToken);
+		}
+	}
 
 	fclose(fp);	//ファイルをクローズする
 
-
 	SAFE_DELETE_ARRAY(buf);	//確保した領域を解放する
+}
+
+/*
+GetToke
+文字列データから、単語を一つ取得する
+*/
+char* CModelX::GetToken() {
+	char* p = mpPointer;
+	char* q = mToken;
+	//タブ(\t)空白()改行()\r(\n),;"の区切り文字以外になるまで読み飛ばす
+	while (*p != '\0' && IsDelimiter(*p)) p++;
+	if (*p == '{' || *p == '}') {
+		//{または}ならmTokenに代入し次の文字へ
+		*q++ = *p++;
+	}
+	else {
+		//タブ(\t)空白()(\r)(\n),;"の区切り文字、
+		//または、}の文字になるまでmTokenに代入する
+		while (*p != '\0' && !IsDelimiter(*p) && *p != '}')
+			*q++ = *p++;
+	}
+
+	*q = '\0';	//mTokenの最後に\0を代入
+	mpPointer = p;	//次の読み込むポイントを更新する
+
+	//もしmTokenが\\の場合は、コメントなので改行するまで読み飛ばす
+	/*
+	strcmp(文字列1、文字列2)
+	文字列１と文字列２が等しい場合、0を返します
+	文字列１と文字列２が等しくない場合、0を返します
+	*/
+	if (!strcmp("//", mToken)) {
+		//改行まで読み飛ばす
+		while (*p != '\0' && !strchr("\r\n", *p)) p++;
+		//読み込み位置の更新
+		mpPointer = p;
+		//単語を取得する(再起呼び出し)
+		return GetToken();
+	}
+	return mToken;
+}
+
+bool CModelX::IsDelimiter(char c)
+{
+	//isspace(c)
+	//cが空白なら0以外を返す
+	if (isspace(c) != 0)
+		return true;
+	/*
+	strchr(文字列、文字)
+	見つかった文字へのポインタを返す
+	見つからなかったらNULLを返す
+	*/
+	if (strchr(",;\"", c) != NULL)
+		return true;
+	//区切り文字ではない
+	return false;
 }
--- 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
@@ -1,6 +1,13 @@
 #ifndef CMODELX_H	//インクルード
 #define CMODELX_H
 
+/*
+IsDelimiter(c)
+cが\t \r \n スペースなどの空白文字
+または,;などの文字であれば
+区切り文字としてtrueを返す
+*/
+
 #define MODEL_FILE "res\\sample.blend.x"	//入力ファイル名
 
 //了以域開放をマクロ化
@@ -16,6 +23,10 @@ public:
 	void Load(char* file);
 private:
 	char* mpPointer;	//読み込み位置
-	char mToken[1024];	//取り出した単語の領域
+	char mToken[1024];	//取り出した単語の領域t
+	//単語の取り出し
+	char* GetToken();
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
 };
 #endif
