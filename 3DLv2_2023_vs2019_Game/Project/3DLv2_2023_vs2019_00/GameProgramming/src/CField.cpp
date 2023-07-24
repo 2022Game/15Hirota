edbfeaead40f4c91067f038c92ecd262349e1c1b
Hirota-Shunya-Fukuyama <gc22017@anabuki-net.ne.jp>
2023-07-24 17:43:04 +0900

    0724

--- 3DLv2_2023_vs2019_Game/Project/3DLv2_2023_vs2019_00/GameProgramming/src/CField.cpp
+++ 3DLv2_2023_vs2019_Game/Project/3DLv2_2023_vs2019_00/GameProgramming/src/CField.cpp
@@ -8,11 +8,19 @@ CField::CField()
 	SetPauseType(TaskPauseType::eGame);
 	//タスクの優先度を設定
 	mPriority = (int)TaskPriority::eBackground;
+
+	//背景のモデルデータ読み込み
+	mpModel = new CModel;
+	mpModel->Load("res\\sky,obj", "res\\sky.mtl");
+	//読み込んだ背景のモデルデータでコライダを作成
+	mColliderMesh.Set(this, &mMatrix, mpModel);
 }
 
 //デストラクタ
 CField::~CField()
 {
+	delete mpModel;
+	mpModel = nullptr;
 }
 
 //更新処理
