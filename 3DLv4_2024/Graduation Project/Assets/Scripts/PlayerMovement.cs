623cb0859d7577bdfd32e470f66ba83bd6f44144
Hirota-Shunya-Fukuyama <113953949+Hirota-Shunya-Fukuyama@users.noreply.github.com>
2024-10-04 10:37:17 +0900

    少し追加

--- 3DLv4_2024/Graduation Project/Assets/Scripts/PlayerMovement.cs
+++ 3DLv4_2024/Graduation Project/Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,13 @@ public class PlayerMovement : MonoBehaviour
     public float speedDampTime = 0.1f;
 
     public Pos2D grid = new Pos2D();
-    public int maxFrame = 100;
+    //public int maxFrame = 100;
 
     private int currentFrame = 0;
     private Pos2D newGrid = null;
+
+    public float maxPerFrame = 1.67f;
+    private float complementFrame;
 
     // readonly : ì«Ç›çûÇ›êÍóp
     private readonly int hashSpeedPara = Animator.StringToHash("Speed");
@@ -22,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        complementFrame = maxPerFrame / Time.deltaTime;
     }
 
     // ï‚ä‘Ç≈åvéZÇµÇƒêiÇﬁ
@@ -33,12 +36,12 @@ public class PlayerMovement : MonoBehaviour
         float px2 =ToWorldX(newPos.x);
         float pz2 = ToWorldZ(newPos.z);
         frame += 1;
-        float t = (float)frame / maxFrame;
+        float t = frame / complementFrame;
         float newX = px1 + (px2 - px1) * t;
         float newZ = pz1 + (pz2 - pz1) * t;
         transform.position = new Vector3(newX, 0, newZ);
         animator.SetFloat(hashSpeedPara,speed,speedDampTime,Time.deltaTime);
-        if (maxFrame == frame)
+        if (complementFrame <= frame)
         {
             frame = 0;
             return newPos;
