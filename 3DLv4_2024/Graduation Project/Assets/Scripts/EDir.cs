fc9f59f4205aee4a147538116d0aacb160db0bd7
Hirota-Shunya-Fukuyama <113953949+Hirota-Shunya-Fukuyama@users.noreply.github.com>
2024-10-04 10:11:56 +0900

    プッシュできていなかったぽいので再プッシュ

---
+++ 3DLv4_2024/Graduation Project/Assets/Scripts/EDir.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EDir
+{
+    Pause, // ê√é~(NULLÇÃë„ÇÌÇË)
+    Up,    // è„
+    Left,  // ç∂
+    Down,  // â∫
+    Right, // âE
+}
---
+++ 3DLv4_2024/Graduation Project/Assets/Scripts/Pos2D.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+// 2ŸŒ³‚ÌÀ•WƒNƒ‰ƒX
+public class Pos2D
+{
+    public int x, z;
+}
