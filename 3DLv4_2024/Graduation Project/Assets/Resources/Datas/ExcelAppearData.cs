39d37cde1df3b21dea62872d532f6ba7a7ea0c1c
Hirota-Shunya-Fukuyama <113953949+Hirota-Shunya-Fukuyama@users.noreply.github.com>
2024-12-05 00:17:12 +0900

    日報を追加

---
+++ 3DLv4_2024/Graduation Project/Assets/Resources/Datas/ExcelAppearData.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class ExcelAppearData : ScriptableObject
+{
+    public List<EnemyAppearData> EnemyAppear;
+    public List<ItemAppearData> ItemAppear;
+
+    [System.Serializable]
+    public class EnemyAppearData
+    {
+        public int floor;
+        public EActor id1;
+        public int lvmin1;
+        public int lvmax1;
+        public int rate1;
+    }
+
+    [System.Serializable]
+    public class ItemAppearData
+    {
+        public EItem id;
+        public int start;
+        public int end;
+        public int rate;
+        public bool shoponly;
+    }
+}
